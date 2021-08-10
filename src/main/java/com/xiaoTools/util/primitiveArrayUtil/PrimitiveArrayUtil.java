7bd444dc84d0bbe47b8eda81b2972f18994aaa4a
xiaoxunyao <huchengyea@163.com>
2021-08-10 17:11:53 +0800

    🥦 init - Array is empty

--- src/main/java/com/xiaoTools/util/primitiveArrayUtil/PrimitiveArrayUtil.java
+++ src/main/java/com/xiaoTools/util/primitiveArrayUtil/PrimitiveArrayUtil.java
@@ -1,5 +1,7 @@
 package com.xiaoTools.util.primitiveArrayUtil;
 
+import com.xiaoTools.lang.constant.Constant;
+
 /**
  * [原始类型数组工具类](Primitive type array utility class)
  * @description: zh - 原始类型数组工具类
@@ -10,5 +12,117 @@ package com.xiaoTools.util.primitiveArrayUtil;
 */
 public class PrimitiveArrayUtil {
 
+    /* 数组是否为空 ------------------------------------------------------------------------------- Is Empty */
 
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:00 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(long[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:01 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(int[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:08 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(char[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:09 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(short[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:10 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(byte[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:10 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(double[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:11 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(float[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
+
+    /**
+     * [数组是否为空](Array is empty)
+     * @description: zh - 数组是否为空
+     * @description: en - Array is empty
+     * @version: V1.0
+     * @author XiaoXunYao
+     * @since 2021/8/10 5:11 下午
+     * @param array: 数组
+     * @return boolean
+    */
+    public static boolean isEmpty(boolean[] array) {
+        return array == Constant.NULL || array.length == Constant.ZERO;
+    }
 }

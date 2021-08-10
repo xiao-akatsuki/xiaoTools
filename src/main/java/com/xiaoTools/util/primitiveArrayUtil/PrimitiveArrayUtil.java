85085d27efc923b0cce4814198406f5d0e74aa1c
xiaoxunyao <huchengyea@163.com>
2021-08-10 16:55:12 +0800

    🥦 init - Primitive type array utility class

---
+++ src/main/java/com/xiaoTools/util/primitiveArrayUtil/PrimitiveArrayUtil.java
@@ -0,0 +1,14 @@
+package com.xiaoTools.util.primitiveArrayUtil;
+
+/**
+ * [原始类型数组工具类](Primitive type array utility class)
+ * @description: zh - 原始类型数组工具类
+ * @description: en - Primitive type array utility class
+ * @version: V1.0
+ * @author XiaoXunYao
+ * @since 2021/8/10 4:53 下午
+*/
+public class PrimitiveArrayUtil {
+
+
+}

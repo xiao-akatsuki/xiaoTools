64d63c489dd216114b104d9475268a8e6ebfa478
xiaoxunyao <huchengyea@163.com>
2021-08-13 13:36:32 +0800

    🌯 init - Split byte array into several equal parts

--- src/main/java/com/xiaoTools/core/versionComparator/VersionComparator.java
+++ src/main/java/com/xiaoTools/core/versionComparator/VersionComparator.java
@@ -2,6 +2,7 @@ package com.xiaoTools.core.versionComparator;
 
 import com.xiaoTools.lang.constant.Constant;
 import com.xiaoTools.util.objectUtil.ObjectUtil;
+import com.xiaoTools.util.strUtil.StrUtil;
 
 import java.io.Serial;
 import java.io.Serializable;

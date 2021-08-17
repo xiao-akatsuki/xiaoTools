0dfe85bbd937caa333e237e1b640c42d96558042
xiaoxunyao <huchengyea@163.com>
2021-08-17 20:03:32 +0800

    int - Check whether the array is in ascending order

--- src/main/java/com/xiaoTools/util/primitiveArrayUtil/PrimitiveArrayUtil.java
+++ src/main/java/com/xiaoTools/util/primitiveArrayUtil/PrimitiveArrayUtil.java
@@ -3431,6 +3431,28 @@ public class PrimitiveArrayUtil {
 		return Constant.TRUE;
 	}
 
+    /**
+     * [检查数组是否升序](Check whether the array is in ascending order)
+     * @description zh - 检查数组是否升序
+     * @description en - Check whether the array is in ascending order
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-08-17 20:03:14
+     * @param array 数组
+     * @return boolean
+     */
+    public static boolean isSortedASC(int[] array) {
+		if (array == Constant.NULL) {
+			return Constant.FALSE;
+		}
+		for (int i = Constant.ZERO; i < array.length - Constant.ONE; i++) {
+			if (array[i] > array[i + Constant.ONE]) {
+				return Constant.FALSE;
+			}
+		}
+		return Constant.TRUE;
+	}
+
     /* 数组是否降序 ------------------------------------------------------------------------------- DESC */
 
     /**
@@ -3474,4 +3496,28 @@ public class PrimitiveArrayUtil {
 		}
 		return Constant.TRUE;
 	}
+
+    /**
+     * [检查数组是否降序](Check whether the array is in descending order)
+     * @description zh - 检查数组是否降序
+     * @description en - Check whether the array is in descending order
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-08-17 20:01:51
+     * @param array 数组
+     * @return boolean
+     */
+    public static boolean isSortedDESC(char[] array) {
+		if (array == Constant.NULL) {
+			return Constant.FALSE;
+		}
+		for (int i = Constant.ZERO; i < array.length - Constant.ONE; i++) {
+			if (array[i] < array[i + Constant.ONE]) {
+				return Constant.FALSE;
+			}
+		}
+		return Constant.TRUE;
+	}
+
+
 }

7849e78f9550764e23edfd5af30a7a71aba30caf
xiaoxunyao <huchengyea@163.com>
2021-08-18 14:32:34 +0800

    Is the array empty

--- src/main/java/com/xiaoTools/util/arrayUtil/ArrayUtil.java
+++ src/main/java/com/xiaoTools/util/arrayUtil/ArrayUtil.java
@@ -1,5 +1,6 @@
 package com.xiaoTools.util.arrayUtil;
 
+import com.xiaoTools.lang.constant.Constant;
 import com.xiaoTools.util.primitiveArrayUtil.PrimitiveArrayUtil;
 
 /**
@@ -8,5 +9,17 @@ import com.xiaoTools.util.primitiveArrayUtil.PrimitiveArrayUtil;
  * @since 2021/5/16 2:21 [下午](afternoon)
 */
 public class ArrayUtil extends PrimitiveArrayUtil {
-    
+    /* 数组是否为空 ------------------------------------------------------------------------------- Is Empty */
+
+    /**
+     * [数组是否为空](Is the array empty)
+     * @description zh - 数组是否为空
+     * @description en - Is the array empty
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-08-18 14:31:59
+     */
+    public static <T> boolean isEmpty(T[] array) {
+		return array == Constant.NULL || array.length == Constant.ZERO;
+	}
 }

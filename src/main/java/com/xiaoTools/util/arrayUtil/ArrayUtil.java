107fa43b91d9ebf7a27a84a7ddcb935c9e7dd529
xiaoxunyao <huchengyea@163.com>
2021-08-18 14:35:15 +0800

    Is the array empty

--- src/main/java/com/xiaoTools/util/arrayUtil/ArrayUtil.java
+++ src/main/java/com/xiaoTools/util/arrayUtil/ArrayUtil.java
@@ -1,5 +1,7 @@
 package com.xiaoTools.util.arrayUtil;
 
+import java.lang.reflect.Array;
+
 import com.xiaoTools.lang.constant.Constant;
 import com.xiaoTools.util.primitiveArrayUtil.PrimitiveArrayUtil;
 
@@ -9,6 +11,7 @@ import com.xiaoTools.util.primitiveArrayUtil.PrimitiveArrayUtil;
  * @since 2021/5/16 2:21 [下午](afternoon)
 */
 public class ArrayUtil extends PrimitiveArrayUtil {
+
     /* 数组是否为空 ------------------------------------------------------------------------------- Is Empty */
 
     /**
@@ -18,8 +21,30 @@ public class ArrayUtil extends PrimitiveArrayUtil {
      * @version V1.0
      * @author XiaoXunYao
      * @since 2021-08-18 14:31:59
+     * @param array 数组
+     * @return boolean
      */
     public static <T> boolean isEmpty(T[] array) {
 		return array == Constant.NULL || array.length == Constant.ZERO;
+	}
+
+    /**
+     * [数组是否为空](Is the array empty)
+     * @description zh - 数组是否为空
+     * @description en - Is the array empty
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-08-18 14:33:32
+     * @param array 数组
+     * @return boolean
+     */
+    public static boolean isEmpty(Object array) {
+		if (array != Constant.NULL) {
+			if (isArray(array)) {
+				return Constant.ZERO == Array.getLength(array);
+			}
+			return Constant.FALSE;
+		}
+		return Constant.TRUE;
 	}
 }

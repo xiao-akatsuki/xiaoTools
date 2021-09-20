a963c3df0859639bdffb262f495911da2d2cdc04
xiaoxunyao <huchengyea@163.com>
2021-09-20 17:58:25 +0800

    Desensitization

--- src/main/java/com/xiaoTools/util/desensitizedUtil/DesensitizedUtil.java
+++ src/main/java/com/xiaoTools/util/desensitizedUtil/DesensitizedUtil.java
@@ -1,5 +1,8 @@
 package com.xiaoTools.util.desensitizedUtil;
 
+import com.xiaoTools.lang.constant.Constant;
+import com.xiaoTools.util.strUtil.StrUtil;
+
 /**
  * [脱敏工具类](Desensitization tools)
  * @description zh - 脱敏工具类
@@ -20,19 +23,39 @@ public class DesensitizedUtil {
     public enum DesensitizedType {
 		//用户id
 		USER_ID,
-		//中文名
-		CHINESE_NAME,
-		//身份证号
-		ID_CARD,
-		//座机号
-		FIXED_PHONE,
-		//手机号
-		MOBILE_PHONE,
-		//地址
-		ADDRESS,
 		//电子邮件
 		EMAIL,
 		//密码
 		PASSWORD
 	}
+
+    /**
+     * [脱敏](Desensitization)
+     * @description zh - 脱敏
+     * @description en - Desensitization
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-09-20 17:56:14
+     */
+    public static String desensitized(CharSequence str, DesensitizedUtil.DesensitizedType desensitizedType) {
+		if (StrUtil.isBlank(str)) {
+			return Constant.EMPTY;
+		}
+		String newStr = String.valueOf(str);
+		switch (desensitizedType) {
+			case USER_ID:
+				newStr = String.valueOf(DesensitizedUtil.userId());
+				break;
+			case EMAIL:
+				newStr = DesensitizedUtil.email(String.valueOf(str));
+				break;
+			case PASSWORD:
+				newStr = DesensitizedUtil.password(String.valueOf(str));
+				break;
+			default:
+		}
+		return newStr;
+	}
+
+
 }

7ca12b20166965e8dd60fd8a562335dd3e91efbd
xiaoxunyao <huchengyea@163.com>
2021-09-21 08:26:03 +0800

    Default initial size

--- src/main/java/com/xiaoTools/util/mapUtil/MapUtil.java
+++ src/main/java/com/xiaoTools/util/mapUtil/MapUtil.java
@@ -1,5 +1,9 @@
 package com.xiaoTools.util.mapUtil;
 
+import com.xiaoTools.lang.constant.Constant;
+
+import org.graalvm.compiler.lir.amd64.AMD64Binary.ConstOp;
+
 /**
  * [Map相关工具类](Map related tool classes)
  * @description zh - Map相关工具类
@@ -9,5 +13,14 @@ package com.xiaoTools.util.mapUtil;
  * @since 2021-09-21 08:24:04
  */
 public class MapUtil {
-    
+
+    /**
+     * [默认初始大小](Default initial size)
+     * @description zh - 默认初始大小
+     * @description en - Default initial size
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-09-21 08:25:38
+     */
+    public static final int DEFAULT_INITIAL_CAPACITY = Constant.SIXTEEN;
 }

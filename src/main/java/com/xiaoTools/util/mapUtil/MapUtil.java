574fb7e31b9858ea3c4d3b4119af5dad493dddd5
xiaoxunyao <huchengyea@163.com>
2021-09-21 08:24:29 +0800

    Map related tool classes

---
+++ src/main/java/com/xiaoTools/util/mapUtil/MapUtil.java
@@ -0,0 +1,13 @@
+package com.xiaoTools.util.mapUtil;
+
+/**
+ * [Map相关工具类](Map related tool classes)
+ * @description zh - Map相关工具类
+ * @description en - Map related tool classes
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-09-21 08:24:04
+ */
+public class MapUtil {
+    
+}

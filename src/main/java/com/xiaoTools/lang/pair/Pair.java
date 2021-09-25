5e173df910d41dd1c7d836ff86357c6c64feb0ac
xiaoxunyao <huchengyea@163.com>
2021-09-25 19:55:20 +0800

    Key value pair object

---
+++ src/main/java/com/xiaoTools/lang/pair/Pair.java
@@ -0,0 +1,18 @@
+package com.xiaoTools.lang.pair;
+
+import com.xiaoTools.lang.clone.cLoneSupport.CloneSupport;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * [键值对对象](Key value pair object)
+ * @description zh - 键值对对象
+ * @description en - Key value pair object
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-09-25 19:54:42
+ */
+public class Pair<K, V> extends CloneSupport<Pair<K, V>> implements Serializable {
+    
+}

1c05fd8d817246d51d05893d68b17f4f2bd6fcd4
xiaoxunyao <huchengyea@163.com>
2021-09-30 14:56:04 +0800

    update

--- src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
+++ src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
@@ -19,5 +19,18 @@ import java.util.function.Function;
  * @since 2021-09-30 14:54:54
  */
 public class MapWrapper<K, V> implements Map<K, V>, Iterable<Map.Entry<K, V>>, Serializable, Cloneable {
+
+    private static final long serialVersionUID = -7524578042008586382L;
+
+    /**
+	 *Default growth factor
+	 */
+	protected static final float DEFAULT_LOAD_FACTOR = 0.75f;
+	/**
+	 * Default initial size
+	 */
+	protected static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; 
+
+	private final Map<K, V> raw;
     
 }

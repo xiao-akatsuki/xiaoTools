2040721567e1331a9648c2ca444bf9f44ba029a1
xiaoxunyao <huchengyea@163.com>
2021-09-30 15:00:57 +0800

    Override

--- src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
+++ src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
@@ -45,4 +45,154 @@ public class MapWrapper<K, V> implements Map<K, V>, Iterable<Map.Entry<K, V>>, S
     public MapWrapper(Map<K, V> raw) {
 		this.raw = raw;
 	}
+
+	@Override
+	public int size() {
+		return raw.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return raw.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return raw.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return raw.containsValue(value);
+	}
+
+	@Override
+	public V get(Object key) {
+		return raw.get(key);
+	}
+
+	@Override
+	public V put(K key, V value) {
+		return raw.put(key, value);
+	}
+
+	@Override
+	public V remove(Object key) {
+		return raw.remove(key);
+	}
+
+	@Override
+	@SuppressWarnings("NullableProblems")
+	public void putAll(Map<? extends K, ? extends V> m) {
+		raw.putAll(m);
+	}
+
+	@Override
+	public void clear() {
+		raw.clear();
+	}
+
+	@Override
+	@SuppressWarnings("NullableProblems")
+	public Collection<V> values() {
+		return raw.values();
+	}
+
+	@Override
+	@SuppressWarnings("NullableProblems")
+	public Set<K> keySet() {
+		return raw.keySet();
+	}
+
+	@Override
+	@SuppressWarnings("NullableProblems")
+	public Set<Entry<K, V>> entrySet() {
+		return raw.entrySet();
+	}
+
+	@Override
+	@SuppressWarnings("NullableProblems")
+	public Iterator<Entry<K, V>> iterator() {
+		return this.entrySet().iterator();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		MapWrapper<?, ?> that = (MapWrapper<?, ?>) o;
+		return Objects.equals(raw, that.raw);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(raw);
+	}
+
+	@Override
+	public String toString() {
+		return raw.toString();
+	}
+
+
+	@Override
+	public void forEach(BiConsumer<? super K, ? super V> action) {
+		raw.forEach(action);
+	}
+
+	@Override
+	public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+		raw.replaceAll(function);
+	}
+
+	@Override
+	public V putIfAbsent(K key, V value) {
+		return raw.putIfAbsent(key, value);
+	}
+
+	@Override
+	public boolean remove(Object key, Object value) {
+		return raw.remove(key, value);
+	}
+
+	@Override
+	public boolean replace(K key, V oldValue, V newValue) {
+		return raw.replace(key, oldValue, newValue);
+	}
+
+	@Override
+	public V replace(K key, V value) {
+		return raw.replace(key, value);
+	}
+
+	@Override
+	public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+		return raw.computeIfAbsent(key, mappingFunction);
+	}
+
+	//---------------------------------------------------------------------------- Override default methods start
+	@Override
+	public V getOrDefault(Object key, V defaultValue) {
+		return raw.getOrDefault(key, defaultValue);
+	}
+
+	@Override
+	public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+		return raw.computeIfPresent(key, remappingFunction);
+	}
+
+	@Override
+	public V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+		return raw.compute(key, remappingFunction);
+	}
+
+	@Override
+	public V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+		return raw.merge(key, value, remappingFunction);
+	}
+	//---------------------------------------------------------------------------- Override default methods end
 }

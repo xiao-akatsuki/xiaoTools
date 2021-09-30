7bc879a5b30f79491118161dd069ec64f415b4de
xiaoxunyao <huchengyea@163.com>
2021-09-30 14:55:20 +0800

    Map wrapper class

--- src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
+++ src/main/java/com/xiaoTools/core/map/mapWrapper/MapWrapper.java
@@ -1,5 +1,23 @@
 package com.xiaoTools.core.map.mapWrapper;
 
-public class MapWrapper {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * [Map包装类](Map wrapper class)
+ * @description zh - Map包装类
+ * @description en - Map wrapper class
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-09-30 14:54:54
+ */
+public class MapWrapper<K, V> implements Map<K, V>, Iterable<Map.Entry<K, V>>, Serializable, Cloneable {
     
 }

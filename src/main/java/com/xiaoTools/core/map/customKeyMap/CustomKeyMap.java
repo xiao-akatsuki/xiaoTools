7be0cc98bc637287d82ffeca0b101eee43561a1b
xiaoxunyao <huchengyea@163.com>
2021-10-02 10:07:01 +0800

    structure

--- src/main/java/com/xiaoTools/core/map/customKeyMap/CustomKeyMap.java
+++ src/main/java/com/xiaoTools/core/map/customKeyMap/CustomKeyMap.java
@@ -1,5 +1,22 @@
 package com.xiaoTools.core.map.customKeyMap;
 
-public class CustomKeyMap<K, V> {
-    
+import java.util.Map;
+
+import com.xiaoTools.core.map.mapWrapper.MapWrapper;
+
+public abstract class CustomKeyMap<K, V> extends MapWrapper<K, V> {
+    private static final long serialVersionUID = 4043263744224569870L;
+
+    /**
+     * [构造](structure)
+     * @description zh - 构造
+     * @description en - structure
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-10-02 10:06:20
+     * @param map 集合
+     */
+    public CustomKeyMap(Map<K, V> map) {
+		super(map);
+	}
 }

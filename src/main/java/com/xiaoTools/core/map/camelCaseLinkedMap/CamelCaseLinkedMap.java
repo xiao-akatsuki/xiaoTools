539d1998b0e40c9b9299c66cd1da341af8051944
xiaoxunyao <huchengyea@163.com>
2021-10-04 08:30:15 +0800

    Hump key style LinkedHashMap

--- src/main/java/com/xiaoTools/core/map/camelCaseLinkedMap/CamelCaseLinkedMap.java
+++ src/main/java/com/xiaoTools/core/map/camelCaseLinkedMap/CamelCaseLinkedMap.java
@@ -1,5 +1,16 @@
 package com.xiaoTools.core.map.camelCaseLinkedMap;
 
-public class CamelCaseLinkedMap<K,V> {
+import com.xiaoTools.core.map.customKeyMap.CustomKeyMap;
+
+/**
+ * [驼峰Key风格的LinkedHashMap](Hump key style LinkedHashMap)
+ * @description zh - 驼峰Key风格的LinkedHashMap
+ * @description en - Hump key style LinkedHashMap
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-10-04 08:28:03
+ */
+public class CamelCaseLinkedMap<K,V> extends CustomKeyMap<K, V> {
     
+    private static final long serialVersionUID = 4043263744224569870L;
 }

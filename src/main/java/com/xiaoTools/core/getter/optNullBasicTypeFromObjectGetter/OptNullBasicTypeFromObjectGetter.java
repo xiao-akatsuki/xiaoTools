ca104f7e41928ad850e1b9ea68d006a036bf500e
xiaoxunyao <huchengyea@163.com>
2021-10-08 07:41:00 +0800

    new

---
+++ src/main/java/com/xiaoTools/core/getter/optNullBasicTypeFromObjectGetter/OptNullBasicTypeFromObjectGetter.java
@@ -0,0 +1,13 @@
+package com.xiaoTools.core.getter.optNullBasicTypeFromObjectGetter;
+
+/**
+ * [基本类型的getter接口抽象实现](Abstract implementation of getter interface of basic type)
+ * @description zh - 基本类型的getter接口抽象实现
+ * @description en - Abstract implementation of getter interface of basic type
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-10-08 07:40:38
+ */
+public class OptNullBasicTypeFromObjectGetter {
+    
+}

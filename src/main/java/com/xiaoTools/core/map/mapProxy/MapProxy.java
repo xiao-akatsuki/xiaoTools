195a9020b26d032cedc8267320c997b4b6388fb9
xiaoxunyao <huchengyea@163.com>
2021-10-08 07:38:36 +0800

    ew

---
+++ src/main/java/com/xiaoTools/core/map/mapProxy/MapProxy.java
@@ -0,0 +1,13 @@
+package com.xiaoTools.core.map.mapProxy;
+
+/**
+ * [Map代理](Map proxy)
+ * @description zh - Map代理
+ * @description en - Map proxy
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-10-08 07:38:06
+ */
+public class MapProxy {
+    
+}

e667745267da2085b168d07093dee083728b100a
xiaoxunyao <huchengyea@163.com>
2021-10-08 07:39:22 +0800

    insert

--- src/main/java/com/xiaoTools/core/map/mapProxy/MapProxy.java
+++ src/main/java/com/xiaoTools/core/map/mapProxy/MapProxy.java
@@ -1,5 +1,13 @@
 package com.xiaoTools.core.map.mapProxy;
 
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * [Map代理](Map proxy)
  * @description zh - Map代理
@@ -8,6 +16,6 @@ package com.xiaoTools.core.map.mapProxy;
  * @author XiaoXunYao
  * @since 2021-10-08 07:38:06
  */
-public class MapProxy {
+public class MapProxy implements Map<Object, Object>, OptNullBasicTypeFromObjectGetter<Object>, InvocationHandler, Serializable {
     
 }

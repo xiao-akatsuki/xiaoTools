9c347a61500d8a8932f1fb0669e5f31e1e3a5b04
xiaoxunyao <huchengyea@163.com>
2021-10-12 09:14:44 +0800

    insert

--- src/main/java/com/xiaoTools/core/getter/basicTypeGetter/BasicTypeGetter.java
+++ src/main/java/com/xiaoTools/core/getter/basicTypeGetter/BasicTypeGetter.java
@@ -10,4 +10,15 @@ package com.xiaoTools.core.getter.basicTypeGetter;
  */
 public interface BasicTypeGetter<K> {
     
+    /**
+     * [获取Object属性值](Get object property value)
+     * @description zh - 获取Object属性值
+     * @description en - Get object property value
+     * @version V1.0
+     * @author XiaoXunYao
+     * @since 2021-10-12 09:13:58
+     * @param key 键
+     * @return java.lang.Object
+     */
+    Object getObj(K key);
 }

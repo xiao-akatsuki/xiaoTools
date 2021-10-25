33bc3ce75437862ad940880fcb7ff142b69110bf
xiaoxunyao <huchengyea@163.com>
2021-10-25 09:14:23 +0800

    new

---
+++ src/main/java/com/xiaoTools/util/beanUtil/BeanUtil.java
@@ -0,0 +1,114 @@
+package com.xiaoTools.util.beanUtil;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import com.xiaoTools.lang.constant.Constant;
+import com.xiaoTools.util.classUtil.ClassUtil;
+
+/**
+ * [Bean工具类](Bean util)
+ * @description zh - Bean工具类
+ * @description en - Bean util
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-10-25 08:56:53
+ */
+public class BeanUtil {
+
+	/**
+	 * [判断是否为可读的Bean对象](Determine whether it is a readable bean object)
+	 * @description zh - 判断是否为可读的Bean对象
+	 * @description en - Determine whether it is a readable bean object
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-10-25 08:59:41
+	 * @param clazz 待测试类
+	 * @return boolean
+	 */
+	public static boolean isReadableBean(Class<?> clazz) {
+		return hasGetter(clazz) || hasPublicField(clazz);
+	}
+
+	/**
+	 * [判断是否有Setter方法](Judge whether there is setter method)
+	 * @description zh - 判断是否有Setter方法
+	 * @description en - Judge whether there is setter method
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-10-25 09:01:59
+	 * @param clazz 类型
+	 * @return boolean
+	 */
+	public static boolean hasSetter(Class<?> clazz) {
+		if (ClassUtil.isNormalClass(clazz)) {
+			final Method[] methods = clazz.getMethods();
+			for (Method method : methods) {
+				if (method.getParameterTypes().length == Constant.ONE && method.getName().startsWith("set")) {
+					return Constant.TRUE;
+				}
+			}
+		}
+		return Constant.FALSE;
+	}
+
+	/**
+	 * [判断是否有Getter方法](Determine whether there is a getter method)
+	 * @description zh - 判断是否有Getter方法
+	 * @description en - Determine whether there is a getter method
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-10-25 09:04:35
+	 * @param clazz 类型
+	 * @return boolean
+	 */
+	public static boolean hasGetter(Class<?> clazz) {
+		if (ClassUtil.isNormalClass(clazz)) {
+			for (Method method : clazz.getMethods()) {
+				if (method.getParameterTypes().length == Constant.ZERO) {
+					if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+						return Constant.TRUE;
+					}
+				}
+			}
+		}
+		return Constant.FALSE;
+	}
+
+	/**
+	 * [指定类中是否有public类型字段](Specifies whether there is a public type field in the class)
+	 * @description zh - 指定类中是否有public类型字段
+	 * @description en - Specifies whether there is a public type field in the class
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-10-25 09:13:11
+	 * @param clazz 类型
+	 * @return boolean
+	 */
+	public static boolean hasPublicField(Class<?> clazz) {
+		if (ClassUtil.isNormalClass(clazz)) {
+			for (Field field : clazz.getFields()) {
+				if (ModifierUtil.isPublic(field) && false == ModifierUtil.isStatic(field)) {
+					return Constant.TRUE;
+				}
+			}
+		}
+		return Constant.FALSE;
+	}
+
+
+
+
+}
